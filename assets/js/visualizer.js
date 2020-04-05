0bec2ab579cf742bc669668daf3bc77c6c84c68c
arunsridher <45627741+arunsridher@users.noreply.github.com>
2020-04-05 22:54:57 +0530

    add search filter

---
+++ assets/js/visualizer.js
@@ -0,0 +1,94 @@
+var tableData;
+$( document ).ready(function() {
+  tableData = undefined;
+});
+
+$('.search').each(function() {
+  var elem = $(this);
+
+  // Save current value of element
+  elem.data('oldVal', elem.val());
+
+  // Look for changes in the value
+  elem.bind("propertychange change click keyup input paste", function(event){
+     // If value has changed...
+     if (elem.data('oldVal') != elem.val()) {
+      // Updated stored value
+      elem.data('oldVal', elem.val());
+
+      // Do action
+      let query = elem.val().toLowerCase();
+      console.log(query);
+
+      //find the table using id
+      var table = $('#data-table');
+
+      //get table data if if table data is empty
+      if(tableData === undefined)
+        tableData = getTableData(table);
+      
+      //filter the data based on query
+      let filteredData = getFilteredTableData(tableData, query);
+
+      //update the table in DOM
+      updateTableData(table, filteredData);
+    }
+  });
+});
+
+// filter table data based on the query string
+function getFilteredTableData(tableData, query) {
+  // loop trough each array and check if any of the columns have the query string
+  // if found add the row to the result
+  let data = [];
+  for(let i = 0; i < tableData.length; i++){
+    let row = tableData[i];
+    let found = false;
+    for(let j = 0; j < row.length; j++){
+      if(row[j].toLowerCase().includes(query)){
+        found = true;
+        break;
+      }
+    }
+    
+    if(found)
+      data.push(row);
+  }
+  return data;
+}
+
+// loop through the table and push the data into an array
+function getTableData(table, query) {
+  let data = [];
+  table.find('tr').each(function (rowIndex, r) {
+      var cols = [];
+      $(this).find('td').each(function (colIndex, c) {
+          cols.push(c.textContent);
+      });
+      data.push(cols);
+  });
+  console.log(data);
+  return data;
+}
+
+//update table data
+function updateTableData(table, data){
+  //variable to hold the new table data
+  let html = '<tbody>';
+  //loop through the data, create rows and columns and append them
+  for(let i = 0; i < data.length; i++){
+    let row = data[i];
+    html += '<tr>';
+    for(let j = 0; j < row.length; j++){
+      html += '<td>'+row[j]+'</td>';
+    }
+    html += '</tr>';
+  }
+  html += '</tbody>';
+
+  //remove old tbody element
+  table.find('tbody').detach();
+
+  //append newly created tbody element
+  table.append(html);
+}
